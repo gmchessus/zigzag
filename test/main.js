778b00645b1cbb283b843707e9764ccca037c648
gmchessus <gmchessus@gmail.com>
2025-06-20 13:44:56 +0300

    Add files via upload

---
+++ test/main.js
@@ -0,0 +1,11 @@
+﻿Ext.onReady(function() {
+	var ceLinks = Ext.select("UL[id=navbar] LI");
+	
+	ceLinks.on("mouseover", function(e, t) {
+	Ext.get(this).addClass("hovered");
+	});
+	
+	ceLinks.on("mouseout", function(e, t) {
+	Ext.get(this).removeClass("hovered");
+	});
+});
